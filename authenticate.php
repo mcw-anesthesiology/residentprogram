d7d98fd3046139d3c548505c390b3cd31e80b616
Jacob Mischka <x.mischka.x@gmail.com>
2014-03-15 05:42:24 -0500

    Properly queries and displays first and last names now

--- authenticate.php
+++ authenticate.php
@@ -9,15 +9,15 @@
 	$username = htmlspecialchars($_POST["username"]);
 	$password = md5(htmlspecialchars($_POST["password"])); //have to encrypt this better
 	
-	$res = $mysqli->query("select id, password, type from users where id='{$username}' and password='{$password}'");
+	$res = $mysqli->query("select id, password, type, firstName, lastName from users where id='{$username}' and password='{$password}'");
 	$row = $res->fetch_assoc();
 	$num = mysqli_num_rows($res);
 	if($num == 1){
 		session_start();
 		$_SESSION["username"] = $username;
 		$_SESSION["type"] = $row["type"];
-		$_SESSION["fname"] = "First";
-		$_SESSION["lname"] = "Last";
+		$_SESSION["fname"] = $row["firstName"];
+		$_SESSION["lname"] = $row["lastName"];
 		header("Location: dashboard.php");
 	}
 	else{
