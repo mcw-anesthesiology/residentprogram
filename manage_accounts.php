e7134969513753c5f05b2d62615268ed5ba31d3f
anthony-j-labuda <labudaaj23@uww.edu>
2014-04-01 10:09:13 -0500

    Fixed Enable\Disable Account
    What it says above

--- manage_accounts.php
+++ manage_accounts.php
@@ -149,10 +149,10 @@ $user = $users->fetch_assoc();
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title" id="myModalDisable">Disable Evaluation</h4>
+          <h4 class="modal-title" id="myModalDisable">Disable Account</h4>
       </div>
       <div class="modal-body">
-        You have selected to <b>disable</b> the selected evaluation. Would you like to continue?
+        You have selected to <b>disable</b> the selected account. Would you like to continue?
       </div>
       <div class="modal-footer modal-disable">
 		<form method="post" action="disable_account.php">
@@ -170,10 +170,10 @@ $user = $users->fetch_assoc();
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title" id="myModalEnable">Enable Evaluation</h4>
+          <h4 class="modal-title" id="myModalEnable">Enable Account</h4>
       </div>
       <div class="modal-body">
-        You have selected to <b>enable</b> the selected evaluation. Would you like to continue?
+        You have selected to <b>enable</b> the selected account. Would you like to continue?
       </div>
       <div class="modal-footer modal-enable">
 		<form method="post" action="enable_account.php">
