f2ad74ec5df85e93b4ee64b7164e4fbe154ccced
Jacob Mischka <x.mischka.x@gmail.com>
2015-01-21 10:18:56 -0600

    password recovery link on login page

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
     <div class="container">
 
       <form class="form-signin" role="form" method="post" action="authenticate.php">
-        <h2 class="form-signin-heading">Please sign in</h2>
+        <h2 class="form-signin-heading">Resident Program</h2>
         <input class="form-control" placeholder="Username" name="username" required autofocus>
         <input type="password" class="form-control" placeholder="Password" name="password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
