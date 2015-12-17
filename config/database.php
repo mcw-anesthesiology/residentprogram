541a11c58b99f0887f442556207e5a0d0e10c7e9
Jacob Mischka <jacobmischka@gmail.com>
2015-12-17 12:52:42 -0600

    Moving to desktop

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('database.sqlite'),
+            'database' => storage_path(env('DB_SQLITE_FILENAME', 'database.sqlite')),
             'prefix'   => '',
         ],
 
