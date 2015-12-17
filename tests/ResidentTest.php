5782b14e94ea6ff84d886af21299535375648788
Jacob Mischka <jacobmischka@gmail.com>
2015-12-17 12:52:42 -0600

    Moving to desktop

--- tests/ResidentTest.php
+++ tests/ResidentTest.php
@@ -6,17 +6,37 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ResidentTest extends TestCase
 {
+	use DatabaseMigrations;
+
     public function setUp(){
         parent::setUp();
 
         $this->user = factory(App\User::class, "resident")->create();
+		$this->faculty = factory(App\User::class, "faculty")->create();
+		$this->form = factory(App\Form::class, "resident")->create();
     }
 
     public function testDashboard(){
         $this->actingAs($this->user)
             ->visit("/")
-            ->see("Evaluate Faculty")
-            ->see("You have no pending evaluations")
-            ->dontSee("tbody");
+            ->see("Account type: Resident")
+            ->see("You have no pending evaluations");
     }
+
+	public function testRequest(){
+		$firstOfMonth = Carbon\Carbon::parse("first day of this month");
+		$this->actingAs($this->user)
+			->visit("/request")
+			->see("Request evaluation")
+			->select($this->faculty->id, "evaluator_id")
+			->select($this->form->id, "form_id")
+			->type($firstOfMonth->format("Y-m-d"), "evaluation_date")
+			->press("Request Evaluation")
+			->seePageIs("/dashboard")
+			->seeInDatabase("evaluations", [
+				"subject_id" => $this->user->id,
+				"evaluator_id" => $this->faculty->id,
+				"form_id" => $this->form->id
+			]);
+	}
 }
