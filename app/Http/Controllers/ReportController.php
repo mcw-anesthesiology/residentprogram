7ea629063f2be9179f338db4bfd44acc78edebec
Jacob Mischka <jacobmischka@gmail.com>
2016-10-26 10:05:14 -0500

    Whoops let non-admins get pdfs for reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -33,7 +33,7 @@ class ReportController extends Controller
     public function __construct(){
         $this->middleware("auth");
         $this->middleware("shared");
-        $this->middleware("type:admin", ["except" => ["specific"]]);
+        $this->middleware("type:admin", ["except" => ["specific", "getPDF"]]);
     }
 
     public function stats($type){
