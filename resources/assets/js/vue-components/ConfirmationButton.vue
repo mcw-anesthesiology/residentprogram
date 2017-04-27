75efc4d3f45b826b319ad387b724524a1b64db82
Jacob Mischka <jacob@mischka.me>
2017-04-27 09:34:29 -0500

    Copy Confirmation components from other branch

---
+++ resources/assets/js/vue-components/ConfirmationButton.vue
@@ -0,0 +1,56 @@
+<template>
+	<button type="button" :class="currentClass" @click="handleClick">
+		<slot v-if="pressed" name="pressed"></slot>
+		<slot v-else></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	props: {
+		class: {
+			type: String,
+			required: false
+		},
+		pressedClass: {
+			type: String,
+			required: false
+		},
+		timeout: {
+			type: Number,
+			required: false,
+			default: 3000
+		}
+	},
+	data(){
+		return {
+			pressed: false
+		};
+	},
+	
+	computed: {
+		currentClass(){
+			return (this.pressedClass && this.pressed)
+				? this.pressedClass
+				: this.class;
+		}
+	},
+		
+	methods: {
+		handleClick(){
+			if(this.pressed){
+				this.$emit('click');
+				this.pressed = false;
+				if(this.pressedTimeout)
+					clearTimeout(this.pressedTimeout);
+			}
+			else {
+				this.pressed = true;
+				this.pressedTimeout = setTimeout(() => {
+					this.pressed = false;
+				}, this.timeout);
+			}
+		}
+	}
+};
+</script>
