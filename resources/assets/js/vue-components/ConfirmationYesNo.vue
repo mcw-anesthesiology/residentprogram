926c1c75f65f110eaf573e2588756cf590e02b0b
Jacob Mischka <jacob@mischka.me>
2017-04-27 09:34:29 -0500

    Copy Confirmation components from other branch

---
+++ resources/assets/js/vue-components/ConfirmationYesNo.vue
@@ -0,0 +1,55 @@
+<template>
+	<span>
+		<button type="button" v-if="!pressed" :class="defaultClass"
+				@click="pressed = true">
+			<slot></slot>
+		</button>
+		
+		
+		<template v-else>
+			<button type="button" :class="yesClass"
+					@click="handleConfirm">
+				<slot name="yes">
+					<span class="glyphicon glyphicon-thumbs-up"></span>
+				</slot>
+			</button>
+			<button type="button" :class="noClass"
+					@click="pressed = false">
+				<slot name="no">
+					<span class="glyphicon glyphicon-thumbs-down"></span>
+				</slot>
+			</button>
+		</template>
+	</span>
+</template>
+
+<script>
+export default {
+	props: {
+		defaultClass: {
+			type: String,
+			required: false
+		},
+		yesClass: {
+			type: String,
+			required: false
+		},
+		noClass: {
+			type: String,
+			required: false
+		}
+	},
+	data(){
+		return {
+			pressed: false
+		};
+	},
+		
+	methods: {
+		handleConfirm(){
+			this.$emit('click');
+			this.pressed = false;
+		}
+	}
+};
+</script>
