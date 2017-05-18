e7e3cfca4eb081e4dffa998ca07d407fb4d32816
Jacob Mischka <jacob@mischka.me>
2017-05-18 09:41:40 -0500

    Make form report font smaller

--- resources/assets/js/modules/report-utils.js
+++ resources/assets/js/modules/report-utils.js
@@ -1,20 +1,20 @@
 import { sortNumbers } from './utils.js';
 
-export function createRadarScaleCallback(valueMap){
+export function createRadarScaleCallback(valueMap) {
 	return value => (valueMap.get(value) || '');
 }
 
-export function createResponseLegend(valueMap){
+export function createResponseLegend(valueMap) {
 	let labels = [];
 	let values = [];
 
 	let keys = Array.from(valueMap.keys()).sort(sortNumbers);
-	
+
 	keys.map(key => {
 		labels.push(valueMap.get(key));
 		values.push(key.toString());
 	});
-	
+
 	return {
 		table: {
 			headerRows: 1,
@@ -26,19 +26,23 @@ export function createResponseLegend(valueMap){
 	};
 }
 
-export function tableHeader(text){
-	return {
-		text: text,
-		style: 'tableHeader'
-	};
+export function pdfmakeStyle(style) {
+	return text => ({
+		text,
+		style
+	});
 }
 
-export function fullWidthTable(table){
+export function tableHeader(text) {
+	return pdfmakeStyle('tableHeader')(text);
+}
+
+export function fullWidthTable(table) {
 	table.widths = Array(table.body[0].length).fill('*');
 	return table;
 }
 
-export function borderedStripedTable(element){
+export function borderedStripedTable(element) {
 	element.layout = {
 		hLineWidth: (i, node) =>
 			i === node.table.headerRows
@@ -47,18 +51,18 @@ export function borderedStripedTable(element){
 		vLineWidth: () => 1,
 		hLineColor: () => '#555',
 		vLineColor: () => '#555',
-		fillColor(i, node){
+		fillColor(i, node) {
 			return (i >= node.table.headerRows
 					&& i % 2 === 1)
 				? '#f3f3f3'
 				: '#fff';
 		}
 	};
-	
+
 	return element;
 }
 
-export function getAverageLevel(average){
+export function getAverageLevel(average) {
 	let level = Math.floor(average) / 2;
 	return level >= 1
 		? `Level ${level}`
@@ -74,10 +78,10 @@ export const sortFunctions = new Map([
 			'ca-3',
 			'fellow'
 		];
-		
+
 		let aLevel = a.training_level.toLowerCase();
 		let bLevel = b.training_level.toLowerCase();
-		
+
 		return sortOrder.indexOf(aLevel) - sortOrder.indexOf(bLevel);
 	}]
 ]);
