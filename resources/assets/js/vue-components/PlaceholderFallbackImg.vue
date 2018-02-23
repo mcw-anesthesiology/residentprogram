92051145d16fe3536d827a1cd355ec2f2a11ee84
Jacob Mischka <jacob@mischka.me>
2018-02-23 14:26:08 -0600

    Do a little work on the detailed egress report page

---
+++ resources/assets/js/vue-components/PlaceholderFallbackImg.vue
@@ -0,0 +1,30 @@
+<template>
+	<img :src="safeSrc" @error="useFallback = true" />
+</template>
+
+<script>
+export default {
+	props: {
+		src: {
+			type: String,
+			required: true
+		},
+		fallback: {
+			type: String,
+			default: '/img/avatar.png'
+		}
+	},
+	data() {
+		return {
+			useFallback: false
+		};
+	},
+	computed: {
+		safeSrc() {
+			return this.useFallback
+				? this.fallback
+				: this.src;
+		}
+	}
+};
+</script>
