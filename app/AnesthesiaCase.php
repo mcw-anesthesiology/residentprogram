22900c0a7def44b1d2cb95e989b651dd9007878d
Jacob Mischka <jacob@mischka.me>
2018-03-01 15:37:51 -0600

    More work on database-based pairing reports

--- app/AnesthesiaCase.php
+++ app/AnesthesiaCase.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Helpers\CaseParser;
+
 class AnesthesiaCase extends Model
 {
     protected $table = 'anesthesia_cases';
@@ -35,6 +37,14 @@ class AnesthesiaCase extends Model
 			'user_anesthesia_cases',
 			'anesthesia_case_id',
 			'user_id'
-		);
+		)->withPivot('start_time', 'stop_time');
+	}
+
+	public static function fromFroedtertEgressRow($row) {
+		return CaseParser::parseFroedtertEgressCase($row);
+	}
+
+	public static function fromCHWTraineeReportRow($row) {
+		return CaseParser::parseCHWTraineeProcedureCase($row);
 	}
 }
