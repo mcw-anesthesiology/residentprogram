fd838a1a8db7acf2816f3ed53519dccc34d3aeb4
Jacob Mischka <jacob@mischka.me>
2018-07-06 15:21:12 -0500

    Add questionnaire exporter
    Needs a little more testing and an interface but I think this is about good?

---
+++ app/Helpers/QuestionnaireExporter.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Helpers;
+
+class QuestionnaireExporter {
+	// This is pretty memory-intensive because they need to be laid out vertically instead of horizontally.
+	// Could maybe use generators instead?
+	static function meritReportsToArray($meritReports) {
+		$header = new QuestionnaireReportHeader($meritReports[0]->report);
+		$sheet = self::addColumn([], $header->toArray());
+
+		foreach ($meritReports as $report) {
+			$col = new QuestionnaireReportColumn($report->report, $report->user->full_name);
+			$sheet = self::addColumn($sheet, $col->toArray());
+		}
+
+		return $sheet;
+	}
+
+	static function addColumn($sheet, $column) {
+		foreach ($column as $i => $cell) {
+			if (empty($sheet[$i]))
+				$sheet[$i] = [];
+
+			$sheet[$i][] = $cell;
+		}
+
+		return $sheet;
+	}
+}
+
+class ReportColumn {
+	protected $column = [];
+	protected $indentLevel = 0;
+
+	protected function getIndent() {
+		return implode('', array_fill(0, $this->indentLevel, "    "));
+	}
+
+	protected function addCell($val) {
+		$this->column[] = $this->getIndent() . $val;
+	}
+
+	public function toArray() {
+		return $this->column;
+	}
+}
+
+class QuestionnaireReportHeader extends ReportColumn {
+
+	public function __construct($report) {
+		$this->column = ['Name'];
+		$this->walkReport($report);
+	}
+
+
+	private function walkReport($report) {
+		foreach ($report['pages'] as $page) {
+			$this->walkSection($page);
+		}
+	}
+
+	private function walkSection($section) {
+		$this->addCell($section['title']);
+		$this->indentLevel++;
+		foreach ($section['items'] as $item) {
+			switch ($item['type']) {
+			case 'section':
+				$this->walkSection($item);
+				break;
+			case 'item':
+				$this->walkItem($item);
+				break;
+			}
+		}
+		$this->indentLevel--;
+	}
+
+	private function walkItem($item) {
+		$this->addCell($item['text']);
+		if (isset($item['questions'])) {
+			$this->indentLevel++;
+			foreach ($item['questions'] as $question) {
+				$this->walkQuestion($question);
+			}
+			$this->indentLevel--;
+		}
+	}
+
+	private function walkQuestion($question) {
+		$this->addCell($question['text']);
+	}
+}
+
+class QuestionnaireReportColumn extends ReportColumn {
+
+	public function __construct($report, $name) {
+		$this->column = [$name];
+		$this->walkReport($report);
+	}
+
+	private function walkReport($report) {
+		foreach ($report['pages'] as $page) {
+			$this->walkSection($page);
+		}
+	}
+
+	private function walkSection($section) {
+		$this->addCell('');
+		foreach ($section['items'] as $item) {
+			switch ($item['type']) {
+			case 'section':
+				$this->walkSection($item);
+				break;
+			case 'item':
+				$this->walkItem($item);
+				break;
+			}
+		}
+	}
+
+	private function walkItem($item) {
+		if (!empty($item['checked'])) {
+			$this->addCell('X');
+			if (isset($item['questions'])) {
+				foreach ($item['questions'] as $question) {
+					$this->walkQuestion($question);
+				}
+			}
+		} else {
+			$this->addCell('');
+			if (isset($item['questions'])) {
+				foreach ($item['questions'] as $question) {
+					$this->addCell('');
+				}
+			}
+		}
+	}
+
+	private function walkQuestion($question) {
+		switch ($question['type']) {
+		case 'text':
+		case 'textarea':
+		case 'number':
+			$this->addCell($question['value']);
+			break;
+		case 'select':
+			$this->addCell(implode(', ', array_map(
+				function($option) {
+					return $option['text'];
+				},
+				array_filter($question['options'], function ($option) {
+					return !empty($option['selected']);
+				})
+			)));
+			break;
+		case 'radio':
+		case 'checkbox':
+			$this->addCell(implode(', ', array_map(
+				function($option) {
+					return $option['text'];
+				},
+				array_filter($question['options'], function ($option) {
+					return !empty($option['checked']);
+				})
+			)));
+			break;
+		case 'list':
+			$this->walkListQuestion($question);
+			break;
+		}
+	}
+
+	private function walkListQuestion($question) {
+		$this->addCell(implode("\n", array_map(
+			function ($itemDisplay) {
+				return '- ' . $itemDisplay;
+			},
+			array_map(function($item) use ($question) {
+				switch ($question['listType']) {
+				case 'text':
+					return $item['text'];
+				default:
+					return self::displayListItemObject($item);
+				}
+			}, $question['items'])
+		)));
+	}
+
+	private static function displayListItemObject($item, $keysToInclude = null) {
+		if (!empty($keysToInclude))
+			$item = array_filter($item, function ($key) use ($keysToInclude) {
+				return in_array($key, $keysToInclude);
+			}, ARRAY_FILTER_USE_KEY);
+
+		return implode("\n", array_map(function ($display) {
+			return "\t" . $display;
+		}, array_map(function ($key, $val) {
+			return ucfirst(camelCaseToWords($key)) . ': ' . $val;
+		}, array_keys($item), $item)));
+	}
+}
+
+
+function camelCaseToWords($str) {
+	$words = preg_split('/[A-Z]/', $str, -1, PREG_SPLIT_NO_EMPTY);
+	if ($words === false)
+		return $str;
+
+	return implode(' ', array_map(function ($word) {
+		return lcfirst($word);
+	}, $words));
+}
