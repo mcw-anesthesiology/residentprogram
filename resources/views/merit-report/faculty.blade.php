8b9f7726edd784e34dbc09374a9ff116dfd878ed
Jacob Mischka <jacob@mischka.me>
2018-10-03 08:35:57 -0500

    Show and send a warning when no merit form found

--- resources/views/merit-report/faculty.blade.php
+++ resources/views/merit-report/faculty.blade.php
@@ -12,58 +12,82 @@
 			font-size: 1.05em;
 			font-weight: bold;
 		}
+
+		#faculty-merit-reports-container .alert {
+			padding: 4em 2em;
+		}
+
+		#faculty-merit-reports-container .alert > .lead {
+			margin: 1em;
+		}
 	</style>
 @endpush
 
-<div id="faculty-merit-reports-container">
-	<div class="container body-block">
-		<template v-if="inProgressReport">
-			<p class="lead">
-				You have a merit report in progress:
-				<span class="progress-form">
-					<span>
-						@{{ inProgressReport.form.name }}
-					</span>
-					-
-					<rich-date-range :dates="inProgressReport"
-						start="period_start"
-						end="period_end" />
+<div id="faculty-merit-reports-container" class="container">
+	<bootstrap-alert v-if="inProgressReport" type="info">
+		<p class="lead">
+			You have a merit checklist in progress:
+			<span class="progress-form">
+				<span>
+					@{{ inProgressReport.form.name }}
 				</span>
-			</p>
-			<div class="btn-lg-submit-container">
-				<button type="button" class="btn btn-lg btn-primary"
-						@click="finishMeritReport">
-					Continue from where you left off
-				</button>
-			</div>
-		</template>
-		<template v-else-if="needsToStartReport">
-			<p class="lead">
-				You haven't submitted a merit report yet for this year, please do that!
-			</p>
-			<div class="btn-lg-submit-container">
-				<button type="button" class="btn btn-lg btn-primary"
-						@click="addMeritReport">
-					Start a new merit checklist
-				</button>
-			</div>
-		</template>
-
-		<template v-else>
-			<p class="lead">
-				You've already completed your merit report for this year. Thanks!
-			</p>
-			<button type="button" class="btn btn-info center-block"
-					@click="viewMostRecentSubmission">
-				View your submission
+				-
+				<rich-date-range :dates="inProgressReport"
+					start="period_start"
+					end="period_end" />
+			</span>
+		</p>
+		<div class="btn-lg-submit-container">
+			<button type="button" class="btn btn-lg btn-primary"
+					@click="finishMeritReport">
+				Continue from where you left off
 			</button>
-		</template>
+		</div>
+	</bootstrap-alert>
+	<template v-else-if="needsToStartReport">
+		<bootstrap-alert v-if="yearlyFacultyForm" type="info">
+			<p class="lead">
+				You haven't submitted a merit checklist yet for this year.
+			</p>
+			<div class="btn-lg-submit-container">
+				<button type="button" class="btn btn-lg btn-primary"
+					@click="addMeritReport">
+						Start a new merit checklist
+				</button>
+			</div>
+		</bootstrap-alert>
+		<bootstrap-alert v-else type="warning">
+			<p class="lead">
+				You haven't submitted a checklist yet for this year, but there was
+				a problem fetching the form. Sorry about that.
+			</p>
+			<p class="lead">
+				Please
+				<a href="/contact?subject=No+merit+form&body=It+says+there+was+a+problem+fetching+the+form+and+to+let+you+know.">
+					let me know using the contact form
+				</a>
+				or by emailing me at
+				<a href="mailto:{{ config('app.admin_email') }}">{{ config('app.admin_email') }}</a>.
+			</p>
+		</bootstrap-alert>
+	</template>
 
-		<alert-list v-model="alerts"></alert-list>
-	</div>
+	<bootstrap-alert v-else type="success">
+		<p class="lead">
+			You've already completed your merit checklist for this year. Thanks!
+		</p>
+		<button type="button" class="btn btn-info center-block"
+				@click="viewMostRecentSubmission">
+			View your submission
+		</button>
+	</bootstrap-alert>
+
+</div>
+
+	<alert-list v-model="alerts"></alert-list>
 
 	<div class="container body-block">
-		<h2>Your merit reports</h2>
+		<h2>Your merit checklists</h2>
 		<component-list v-if="userMeritReports && userMeritReports.length > 0"
 				:items="userMeritReports"
 				:fields="meritReportFields"
@@ -82,8 +106,7 @@
 		</component-list>
 		<div v-else>
 			<p class="lead">
-				You don't have any submitted merit reports yet.
-				Please submit one!
+				You don't have any submitted merit checklists yet.
 			</p>
 		</div>
 	</div>
