fb2adf63ec2c323f8211883f9fc1ecaa5313e475
Jacob Mischka <jacob@mischka.me>
2018-10-04 13:43:41 -0500

    Add administrators to user groups

--- resources/assets/js/modules/utils.js
+++ resources/assets/js/modules/utils.js
@@ -322,6 +322,10 @@ export function groupUsers(users: Array<User>): Array<Select2OptGroup> {
 			text: 'APP',
 			children: []
 		},
+		admin: {
+			text: 'Administrator',
+			children: []
+		},
 		inactive: {
 			text: 'Inactive',
 			children: []
