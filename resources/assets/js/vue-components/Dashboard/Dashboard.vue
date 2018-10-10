e286b56dd1aac5821676b5cb6d17d94b88193234
Jacob Mischka <jacob@mischka.me>
2018-10-09 21:01:53 -0500

    Start on admin dashboard
    Didn't make much progress, I'll work on it tomorrow

--- resources/assets/js/vue-components/Dashboard/Dashboard.vue
+++ resources/assets/js/vue-components/Dashboard/Dashboard.vue
@@ -1,47 +1,14 @@
 <template>
-	<div class="dashboard" :class="`${user ? user.type : ''}`">
-		<div v-if="subjectEvaluations" class="container body-block subject-evaluations-block">
-			<h2>
-				My evaluations
-				<button type="button" class="btn btn-sm btn-default" @click="$store.dispatch('evaluations/subject/fetch', dates)">
-					<span class="glyphicon glyphicon-refresh"></span>
-				</button>
-			</h2>
-			<subject-evaluations-block :evaluations="subjectEvaluations"></subject-evaluations-block>
-		</div>
-
-		<div v-if="evaluatorEvaluations" class="container body-block evaluator-evaluations-block">
-			<h2>
-				Evaluations for me to complete
-				<button type="button" class="btn btn-sm btn-default" @click="$store.dispatch('evaluations/evaluator/fetch', dates)">
-					<span class="glyphicon glyphicon-refresh"></span>
-				</button>
-			</h2>
-			<evaluator-evaluations-block :evaluations="evaluatorEvaluations"></evaluator-evaluations-block>
-		</div>
+	<div>
+		<component :is="dashboard" :dates="dates" />
 	</div>
 </template>
-
-<style scoped>
-	.dashboard {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.dashboard.faculty.evaluator-evaluations-block {
-		order: 1;
-	}
-
-	.dashboard.faculty.subject-evaluations-block {
-		order: 2;
-	}
-</style>
 
 <script>
 import { mapState } from 'vuex';
 
-import SubjectEvaluationsBlock from './SubjectEvaluationsBlock.vue';
-import EvaluatorEvaluationsBlock from './EvaluatorEvaluationsBlock.vue';
+import AdminDashboard from './AdminDashboard.vue';
+import UserDashboard from './UserDashboard.vue';
 
 export default {
 	props: {
@@ -52,30 +19,9 @@ export default {
 	},
 	computed: {
 		...mapState(['user']),
-		subjectEvaluations() {
-			return this.$store.getters['evaluations/subject/listBetween'](this.dates);
-		},
-		evaluatorEvaluations() {
-			return this.$store.getters['evaluations/evaluator/listBetween'](this.dates);
+		dashboard() {
+			return this.user && this.user.type === 'admin' ? AdminDashboard : UserDashboard;
 		}
-	},
-	mounted() {
-		this.fetchEvaluations();
-	},
-	watch: {
-		dates() {
-			this.fetchEvaluations();
-		}
-	},
-	methods: {
-		fetchEvaluations() {
-			this.$store.dispatch('evaluations/subject/fetch', this.dates);
-			this.$store.dispatch('evaluations/evaluator/fetch', this.dates);
-		}
-	},
-	components: {
-		SubjectEvaluationsBlock,
-		EvaluatorEvaluationsBlock
 	}
 };
 </script>
