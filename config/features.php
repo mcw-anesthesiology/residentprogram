41d311c776bad22c9ec72cc8b28b1f67dc7dbb24
Jacob Mischka <jacob@mischka.me>
2018-10-17 16:03:23 -0500

    Start on external evals and users for Global Health

--- config/features.php
+++ config/features.php
@@ -6,6 +6,7 @@ return [
 	'faculty_evaluations' => env('FEATURE_FACULTY_EVALS', true),
 	'app_evaluations' => env('FEATURE_APP_EVALS', true),
 	'intern360_evaluations' => env('FEATURE_INTERN360_EVALS', true),
+	'external_evaluations' => env('FEATURE_EXTERNAL_EVALS', true),
 
 	'faculty_merit' => env('FEATURE_FACULTY_MERIT', true),
 	'case_log' => env('FEATURE_CASE_LOG', true),
