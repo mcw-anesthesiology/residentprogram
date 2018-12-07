b96988b52113316cf8a44f240fce8ea55f50ce36
Jacob Mischka <jacob@mischka.me>
2018-12-07 15:03:41 -0600

    Add small message for unexpiring links

--- resources/views/evaluations/saved.blade.php
+++ resources/views/evaluations/saved.blade.php
@@ -6,7 +6,9 @@
 		Your evaluation has been saved.
 	</p>
 
-@if (!empty($evalExpiration))
+@if (empty($evalExpiration))
+	<p>You may return to the evaluation using the same link to complete it later.</p>
+@else
 	<p>
 		You can return to the evaluation using the link to complete it before it expires.
 		The evaluation link is set to expire on <b>{{ $evalExpiration }}</b>.
