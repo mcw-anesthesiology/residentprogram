e9827f654d6549bc7569e782627947c2903837c3
Jacob Mischka <jacob@mischka.me>
2019-03-05 15:20:54 -0600

    Only fetch users with merit reports

--- resources/assets/js/vue-components/Reports/FacultyMerit/Simulations.vue
+++ resources/assets/js/vue-components/Reports/FacultyMerit/Simulations.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="container body-block">
 		<p v-if="$apollo.loading">Loading...</p>
-		<div v-else-if="usersWithMerit">
+		<div v-else-if="usersWithMerits">
 			<data-table :thead="thead" :data="userParticipates"
 				:export-filename="exportFilename"
 				reloadable
 				exportable
-				@reload="fetchUsersWithMerits" />
+				@reload="$apollo.queries.users.refetch()" />
 		</div>
 	</div>
 </template>
@@ -16,7 +16,7 @@ import gql from 'graphql-tag';
 
 import DataTable from '@/vue-components/DataTable.vue';
 
-import { logError } from '@/modules/errors.js';
+import { logError, storeError } from '@/modules/errors.js';
 import { isoDateString } from '@/modules/date-utils.js';
 
 export default {
@@ -28,18 +28,22 @@ export default {
 
 	data() {
 		return {
-			users: []
+			usersWithMerits: []
 		};
 	},
 	apollo: {
-		users: {
+		usersWithMerits: {
 			query: gql`
 				query SimulationsQuery(
 					$formId: ID
 					$startDate: String
 					$endDate: String
 				) {
-					users {
+					usersWithMerits(
+						form_id: $formId
+						period_start: $startDate
+						period_end: $endDate
+					) {
 						id
 						full_name
 						meritReports(
@@ -59,14 +63,14 @@ export default {
 					formId: this.formId,
 					status: this.completeOnly ? 'COMPLETE' : null
 				};
+			},
+			error(err) {
+				storeError(err, this, 'Sorry, there was a problem fetching the report');
 			}
 		}
 	},
 
 	computed: {
-		usersWithMerit() {
-			return this.users.filter(u => u.meritReports.length > 0);
-		},
 		thead() {
 			return [[
 				'Faculty member',
@@ -74,10 +78,10 @@ export default {
 			]];
 		},
 		userParticipates() {
-			if (!this.usersWithMerit)
+			if (!this.usersWithMerits)
 				return;
 
-			return this.usersWithMerit.map(user => {
+			return this.usersWithMerits.map(user => {
 				let participates = '';
 				try {
 					if (user.meritReports[0].participatesInSimulation) {
