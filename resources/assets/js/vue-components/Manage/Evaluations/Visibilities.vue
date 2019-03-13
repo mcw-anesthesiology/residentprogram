938020508f1ba20529be0a3cb4652e4945b6e25f
Jacob Mischka <jacob@mischka.me>
2019-03-13 15:09:17 -0500

    Add evaluation visibility management

---
+++ resources/assets/js/vue-components/Manage/Evaluations/Visibilities.vue
@@ -0,0 +1,149 @@
+<template>
+	<div class="manage-visibilities container body-block">
+		<h2>Manage visibilities</h2>
+
+		<form @submit="handleSubmit">
+			<div class="form-group">
+				<label class="containing-label">
+					Evaluation completion date
+					<start-end-date v-model="dates" />
+				</label>
+			</div>
+
+			<div class="form-group">
+				<label class="containing-label">
+					Visibility
+					<select class="form-control" v-model="visibility">
+						<option :value="null">(Form default)</option>
+						<option v-for="visibility of VISIBILITIES" :value="visibility">
+						{{ ucfirst(visibility) }}
+						</option>
+					</select>
+				</label>
+			</div>
+
+			<div class="btn-lg-submit-container">
+				<button type="submit" class="btn btn-lg btn-primary">
+					Update visiblities
+				</button>
+			</div>
+		</form>
+
+		<div v-if="updatedEvaluations">
+			<bootstrap-alert v-if="updatedEvaluations.length === 0" type="warning">
+				<p>No evaluations found in the selected date range.</p>
+			</bootstrap-alert>
+			<div v-else class="panel panel-success">
+				<div class="panel-heading">
+					<h3 class="panel-title">Updated evaluations</h3>
+				</div>
+				<div class="panel-body">
+					<evaluation-list :evaluations="updatedEvaluations" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.panel-success {
+		margin: 2em;
+	}
+</style>
+
+<script>
+import gql from 'graphql-tag';
+
+import StartEndDate from '#/StartEndDate.vue';
+import BootstrapAlert from '#/BootstrapAlert.vue';
+
+import { storeError } from '@/modules/errors.js';
+import { isoDateStringObject, thisMonth } from '@/modules/date-utils.js';
+import { ucfirst } from '@/modules/text-utils.js';
+
+const VISIBILITIES = [
+	'visible',
+	'hidden',
+	'anonymous'
+];
+
+export default {
+	data() {
+		return {
+			dates: isoDateStringObject(thisMonth()),
+			visibility: null,
+
+			updatedEvaluations: null,
+
+			VISIBILITIES
+		};
+	},
+	methods: {
+		ucfirst,
+		handleSubmit() {
+			this.updatedEvaluations = null;
+
+			this.$apollo.mutate({
+				mutation: gql`
+					mutation ManageEvaluationVisibilities(
+						$startDate: Date!
+						$endDate: Date!
+						$visibility: Visibility
+					) {
+						updatedEvaluations: updateEvaluationVisibilities(
+							startDate: $startDate
+							endDate: $endDate
+							visibility: $visibility
+						) {
+							id
+							type
+							request_date
+							complete_date
+							visibility
+							comment
+							status
+							evaluation_date_start
+							evaluation_date_end
+
+							form_id
+							form {
+								id
+								title
+							}
+							subject_id
+							subject {
+								id
+								full_name
+							}
+							evaluator_id
+							evaluator {
+								id
+								full_name
+							}
+							requested_by_id
+							requestor {
+								id
+								full_name
+							}
+						}
+					}
+				`,
+				variables: {
+					...this.dates,
+					visibility: this.visibility
+				}
+			}).then(({ data: { updatedEvaluations } }) => {
+
+				this.updatedEvaluations = updatedEvaluations;
+			}).catch(err => {
+				storeError(err, this, 'There was a problem updating visibilities');
+			});
+		}
+	},
+	components: {
+		BootstrapAlert,
+		StartEndDate,
+		EvaluationList: () => import('#/EvaluationList.vue')
+	}
+};
+</script>
