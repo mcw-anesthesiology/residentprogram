9186488a48e91cc308604ff6bd2ccd87a0a8bf06
Jacob Mischka <jacob@mischka.me>
2019-05-07 11:42:14 -0500

    Make checklist status more obvious, fix autosave

---
+++ resources/assets/js/vue-components/MeritCompensation/Checklist/SaveStatus.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="report-status">
+		<bootstrap-alert v-if="unsaved" type="warning">
+			<p>
+				You have unsaved changes
+				<button type="button" class="btn btn-info btn-xs unsaved-button" :disabled="saving" @click="$emit('save')">
+					{{ saving ? 'Saving...' : 'Save' }}
+				</button>
+			</p>
+		</bootstrap-alert>
+		<bootstrap-alert v-else type="success">
+			<p>All changes saved successfully</p>
+		</bootstrap-alert>
+	</div>
+</template>
+
+<script>
+import BootstrapAlert from '#/BootstrapAlert.vue';
+
+export default {
+	props: {
+		unsaved: Boolean,
+		saving: Boolean,
+	},
+	components: {
+		BootstrapAlert
+	}
+};
+</script>
