e32b1ae9cd2394f195257ae4713ff774412adb44
Jacob Mischka <jacob@mischka.me>
2019-07-12 14:26:47 -0500

    Add resolvers to get responses
    Gonna use it for the dashboard to get a response from the faculty form

---
+++ app/Http/GraphQL/Queries/ResponseSummary.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\GraphQL\Queries;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+use App\Evaluation;
+use App\Response;
+
+use App\Helpers\Math;
+
+class ResponseSummary
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
+		$evaluationIds = Evaluation::where([
+			'form_id' => $args['formId'],
+			'subject_id' => $args['subjectId'],
+			'status' => 'complete'
+		])->get(['id'])->pluck(['id']);
+
+		$responses = Response::where('question_id', $args['questionId'])->whereIn('evaluation_id', $evaluationIds)->get();
+
+		return [
+			'responses' => $responses,
+			'values' => $responses->pluck('response')->toArray()
+		];
+    }
+
+	public function resolveAverage($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
+		return Math::mean($rootValue['values']);
+	}
+
+	public function resolveStdDev($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
+		return Math::sd($rootValue['values']);
+	}
+
+	public function resolveNum($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
+		return count($rootValue['responses']);
+	}
+}
