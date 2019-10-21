768d6bf45e9d05b1f1d18e9c1cc4b37a760f2d1d
Jacob Mischka <jacob@mischka.me>
2019-10-21 14:51:28 -0500

    Should really gzip these db backups.

--- resources/scripts/backup-db.sh
+++ resources/scripts/backup-db.sh
@@ -12,6 +12,6 @@ BOX_EXTERNAL_PASSWORD=98sNU9FJTXFXIdFu
 BOX_DIRECTORY=residentprogram-backups
 
 DB_BACKUP_DIRECTORY=db
-BACKUP_FILENAME=$DATE.db
+BACKUP_FILENAME=$DATE.db.gz
 
-mysqldump $DATABASE --user="$DB_USERNAME" --password="$DB_PASSWORD" $DB_DATABASE | curl -1 --ftp-ssl --disable-epsv --ftp-skip-pasv-ip -u $BOX_USERNAME:$BOX_EXTERNAL_PASSWORD --upload-file /dev/stdin ftp://ftp.box.com/$BOX_DIRECTORY/$DB_BACKUP_DIRECTORY/$BACKUP_FILENAME
+mysqldump --user="$DB_USERNAME" --password="$DB_PASSWORD" $DB_DATABASE | gzip -c | curl -1 --ftp-ssl --disable-epsv --ftp-skip-pasv-ip -u $BOX_USERNAME:$BOX_EXTERNAL_PASSWORD --upload-file /dev/stdin ftp://ftp.box.com/$BOX_DIRECTORY/$DB_BACKUP_DIRECTORY/$BACKUP_FILENAME
