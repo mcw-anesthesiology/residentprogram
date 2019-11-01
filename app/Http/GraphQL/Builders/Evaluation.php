6a2dccb968052ec315e4c66722269f4dbb47cbf0
Jacob Mischka <jacob@mischka.me>
2019-11-01 10:59:24 -0500

    Lots of qol tweaks to evaluator report

--- app/Http/GraphQL/Builders/Evaluation.php
+++ app/Http/GraphQL/Builders/Evaluation.php
@@ -6,7 +6,11 @@ class Evaluation {
 	public function subjectFilter($builder, $subjectFilter) {
 		return $builder->whereHas('subject', function($query) use ($subjectFilter) {
 			foreach ($subjectFilter as $key => $val) {
-				$query->where($key, $val);
+				if ($key == 'training_level' && $val == 'RESIDENT') {
+					$query->whereIn($key, config('constants.RESIDENT_TRAINING_LEVELS'));
+				} else {
+					$query->where($key, $val);
+				}
 			}
 
 			return $query;
