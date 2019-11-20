841a37bef9610b906c7ceda16bfd87d399aa6591
Jacob Mischka <jacob@mischka.me>
2019-11-20 15:00:42 -0600

    Add R², standard error, and point counts to plots

--- resources/scripts/plot_subspecialties.py
+++ resources/scripts/plot_subspecialties.py
@@ -31,34 +31,52 @@ def main():
         plt.figure()
         plt.xlabel("Tradiational response average")
         plt.ylabel("Scenario response average")
-        plot(rows, "response_avg", "scenario_avg")
+        r_value, _p_value, std_err, points = plot(rows, "response_avg", "scenario_avg")
+        plt.suptitle(
+            "{} (Avg # evals per person={})".format(
+                subspecialty, round(average([int(row["num_evals"]) for row in rows]), 2)
+            )
+        )
         plt.title(
+            "(R²={}, StdErr={}, #={})".format(
+                round(r_value ** 2, 5), round(std_err, 5), len(points),
+            )
+        )
+        plt.tight_layout(pad=2)
+        if outdir is not None:
+            plt.savefig(
+                os.path.join(outdir, "{}-All.png".format(subspecialty.replace("/", "")))
+            )
+
+        plt.figure()
+        plt.suptitle(
             "{} (Average # of evaluations per person: {})".format(
                 subspecialty, round(average([int(row["num_evals"]) for row in rows]), 2)
             )
         )
-        plt.tight_layout()
-
-        plt.figure()
         plt.subplot(2, 1, 1)
         plt.xlabel("Tradiational response average")
         plt.ylabel("Basic scenario average")
-        plot(rows, "response_avg", "basic_scenario_response_avg")
+        r_value, _p_value, std_err, points = plot(
+            rows, "response_avg", "basic_scenario_response_avg"
+        )
         plt.title(
-            "{} (Average # of evaluations per person: {})".format(
-                subspecialty, round(average([int(row["num_evals"]) for row in rows]), 2)
+            "(R²={}, StdErr={}, #={})".format(
+                round(r_value ** 2, 5), round(std_err, 5), len(points)
             )
         )
         plt.subplot(2, 1, 2)
         plt.xlabel("Tradiational response average")
         plt.ylabel("Advanced scenario average")
-        plot(rows, "response_avg", "advanced_scenario_response_avg")
+        r_value, _p_value, std_err, points = plot(
+            rows, "response_avg", "advanced_scenario_response_avg"
+        )
         plt.title(
-            "{} (Average # of evaluations per person: {})".format(
-                subspecialty, round(average([int(row["num_evals"]) for row in rows]), 2)
+            "(R²={}, StdErr={}, #={})".format(
+                round(r_value ** 2, 5), round(std_err, 5), len(points)
             )
         )
-        plt.tight_layout()
+        plt.tight_layout(pad=2)
 
         if outdir is not None:
             plt.savefig(
